cce0164ac97a9772a361067ac898418a0175f5b4
JoAnna Scott-Scates <joanna.scott.uky@gmail.com>
2024-11-11 22:34:15 -0500

    Code complete

---
+++ data.js
@@ -0,0 +1,71 @@
+const database = {
+    recipes: [
+        {
+            rName: 'Mushroom Rice Balls',
+            ingOne: 'Hylian Rice',
+            ingTwo: 'Big Hearty Truffle'
+        },{
+            rName: 'Cheesy Omlette',
+            ingOne: 'Hateno Cheese',
+            ingTwo: 'Bird Egg'
+        },{
+            rName: 'Cheesy Hylian Pizza',
+            ingOne: 'Tabantha Wheat',
+            ingTwo: 'Hateno Cheese'
+        }, {
+            rName: 'Prime Meat and Rice Bowl',
+            ingOne: 'Raw Prime Meat',
+            ingTwo: 'Hylian Rice'
+        }, {
+            rName:'Egg Pudding',
+            ingOne: 'Fresh Milk',
+            ingTwo: 'Cane Sugar',
+            ingThree: 'Bird Egg'
+        }, {
+            rName:'Egg Tart',
+            ingOne: 'Tabantha Wheat',
+            ingTwo: 'Cane Sugar',
+            ingThree: 'Goat Butter',
+            ingFour: 'Bird Egg'
+        }, {
+            rName: 'Chicken Egg Fried Rice',
+            ingOne: 'Hylian Rice',
+            ingTwo: 'Raw Prime Meat',
+            ingThree: 'Bird Egg'
+        },{
+            rName:'Poultry Pilaf',
+            ingOne:'Hylian Rice',
+            ingTwo: 'Goat Butter',
+            ingThree: 'Bird Egg',
+            ingFour: 'Raw Bird Thigh'
+        }
+    ],
+    materials: [
+        {
+            matName: 'Hylian Rice'
+        },{
+            matName: 'Big Hearty Truffle'
+        },{
+            matName: 'Tabantha Wheat'
+        },{
+            matName: 'Raw Prime Meat'
+        }, {
+            matName: 'Hateno Cheese'
+        },{
+            matName: 'Bird Egg'
+        },{
+            matName: 'Goat Butter'
+        },{
+            matName: 'Fresh Milk'
+        },{
+            matName: 'Cane Sugar'
+        },{
+            matName: 'Raw Bird Thigh'
+        }
+    ]
+}
+
+module.exports = {
+    database
+}
+
--- main.js
+++ main.js
@@ -1,18 +1,53 @@
-const materials = ['Hylian Rice', 'Big Hearty Truffle', 'Tabantha Wheat', 'Raw Prime Meat', 'Hateno Cheese', 'Bird Egg']
+const {
+    database
+} = require("./data.js")
+
+//removed the const materials, moved it to the database const under the materials array.
 const meals = []
 
+
+//looks at the firstIngredient and compares it to materials array
+//loo at the secondIngredient and compare it to the materials array
+//if the two ingredients make a recipe, push the recipe name
+/* rewriting into own function
 const cook = (firstIngredient, secondIngredient) => {
     if (firstIngredient === 'Hylian Rice' && secondIngredient === 'Big Hearty Radish') {
         meals.push('Mushroom Rice Balls')
     }
+}*/
+
+
+const cook = (firstIngredient, secondIngredient, thirdIngredient = null, fourthIngredient=null) => {
+    for (const recipe of database.recipes) {
+        let firstIngCheck = firstIngredient === recipe.ingOne || firstIngredient === recipe.ingTwo || firstIngredient === recipe.ingThree || firstIngredient === recipe.ingFour
+
+        let secondIngCheck = secondIngredient === recipe.ingOne || secondIngredient === recipe.ingTwo || secondIngredient === recipe.ingThree || secondIngredient === recipe.ingFour
+
+        let thirdIngCheck = thirdIngredient === recipe.ingOne || thirdIngredient === recipe.ingTwo || thirdIngredient === recipe.ingThree || thirdIngredient === recipe.ingFour
+
+        let fourthIngCheck = fourthIngredient === recipe.ingOne || fourthIngredient === recipe.ingTwo || fourthIngredient === recipe.ingThree || fourthIngredient === recipe.ingFour
+        
+        if(firstIngCheck && secondIngCheck) {
+            let dishName = recipe.rName
+            meals.push(dishName)
+        }
+        if (thirdIngredient && firstIngCheck && secondIngCheck && thirdIngCheck) {
+            let dishName = recipe.rName
+            meals.push(dishName)
+        }
+        if (fourthIngredient && firstIngCheck && secondIngCheck && thirdIngCheck && fourthIngCheck){
+            let dishName = recipe.rName
+            meals.push(dishName) 
+        }
+    }
 }
 
-cook(materials[1], materials[0])
+cook(database.materials[7].matName, database.materials[8].matName, database.materials[6].matName)
 
 console.log('MEALS:')
 console.log('---------------')
 for (const meal of meals) {
-    console.log(meel)
+    console.log(meal)
 }
 
 
